fedb2875c218de6141ff677720d392dee8990ddd
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-19 14:59:46 +0200

    Add: method to create random card Add: method to print list of cards

--- EnumsAndCollectionsExercise04/Program.cs
+++ EnumsAndCollectionsExercise04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnumsAndCollectionsExercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,19 @@ namespace EnumsAndCollectionsExercise04
 {
     internal class Program
     {
+        private static Random random = new Random();
         static void Main(string[] args)
         {
+        }
+
+        public static Card RandomCard()
+        {
+            return new Card((Values)random.Next(1,14), (Suits)random.Next(4));
+        }
+        public static void PrintCards(List<Card> cards)
+        {
+            foreach(Card card in cards)
+                Console.WriteLine(card.Name);
         }
     }
 }
